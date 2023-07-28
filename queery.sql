3e5e84a92c5da190983c53ef234fe2d6d6ff6d5b
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-28 16:30:14 +0200

    groupby

--- queery.sql
+++ queery.sql
@@ -21,3 +21,18 @@ SELECT COUNT(*) FROM `degrees` WHERE `level` = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 SELECT COUNT(*) FROM `departments`;
+
+
+
+--  groupby
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
