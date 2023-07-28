aacbc2134d34441701c9590a7e41ce34eb75ec85
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-28 16:38:35 +0200

    1 groupby

--- queery.sql
+++ queery.sql
@@ -27,7 +27,7 @@ SELECT COUNT(*) FROM `departments`;
 --  groupby
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS `studenti`, `enrolment_date` AS `iscrizione` FROM `students` GROUP BY `enrolment_date`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
