ed521c19303f34f4824435c2a22b096921e87aaf
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-31 16:53:10 +0200

    5 Queries con JOIN

--- queery.sql
+++ queery.sql
@@ -60,6 +60,6 @@ SELECT S.`name`, S.`surname` ,DEG.`name` , D.`name` FROM `students` AS S JOIN `d
 SELECT d.`name`, C.`name` , T.`name` , T.`surname` FROM `degrees` AS D JOIN `courses` AS C ON D.`id` = C.`degree_id` JOIN `course_teacher` AS CT ON C.`id` = CT.`course_id` JOIN `teachers` as T ON T.`id` = ct.`teacher_id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-
+SELECT T.`name`, T.`surname`, D.`name` FROM `teachers` AS T JOIN `course_teacher` AS CT ON T.`id` = CT.`teacher_id` JOIN `courses` AS C ON C.`id` = CT.`course_id` JOIN `degrees` AS DEG ON DEG.`id` = C.`degree_id` JOIN `departments` AS D on D.`id` = DEG.`department_id` WHERE D.`name` = 'Dipartimento di Matematica';
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esamI
