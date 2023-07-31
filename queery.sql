4e89af2681386a152d4b4285241c151ac7b1f804
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-31 15:23:04 +0200

    START

--- queery.sql
+++ queery.sql
@@ -37,3 +37,29 @@ SELECT AVG(`vote`) AS `media`, `exam_id` AS `esame` FROM `exam_student` GROUP BY
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(`name`) AS `corsi` , `department_id` AS `dipartimento` FROM `degrees` GROUP BY `dipartimento`;
+
+
+
+
+-- Queries con JOIN
+
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esamI
